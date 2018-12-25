e1fb6373294eb235ec76bc0902f8a88c204d3b87
magzupao <mguado@gmail.com>
2018-12-25 22:53:53 +0100

    mejoras en el codigo

--- cachematrix.R
+++ cachematrix.R
@@ -4,30 +4,31 @@
 # Marco Guado Zavaleta, mguado@gmail.com                   #
 #==========================================================#
 
-makeCacheMatrix <- function(x = matrix()) {
-	m<-NULL
-	set<-function(y){
-		x<<-y
-		m<<-NULL
-	}
-	get<-function() x
-	setmatrix<-function(solve) m<<- solve
-	getmatrix<-function() m
-	list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m<-x$getmatrix()
-    if(!is.null(m)){
-		message("getting cached data")
-		return(m)
-    }
-    matrix<-x$get()
-    m<-solve(matrix, ...)
-    x$setmatrix(m)
-    m        
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
